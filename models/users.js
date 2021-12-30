0534458ccccf1042f96dad4129a5bbb8be2a2ead
justineicher <justineicher@gmail.com>
2021-12-29 21:20:59 -0600

    Users model created

---
+++ models/users.js
@@ -0,0 +1,36 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Users extends Model {}
+
+// set up fields and rules for Users model
+Users.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "users",
+  }
+);
+
+module.exports = Users;
