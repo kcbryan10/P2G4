908fd620c630c57c1ebdf050a9b8908b2493290a
Daniel Denton <danielryandenton@gmail.com>
2022-01-03 15:32:14 -0600

    install and utilize luxon

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,36 +1,82 @@
 const router = require('express').Router();
 const { Lesson, Student, Teacher } = require('../models');
 const withAuth = require('../utils/auth');
+const { DateTime } = require('luxon');
 
-router.get('/', withAuth, (req, res) => {
-  Lesson.findAll({
-    where: {
-      student_id: req.session.student_id,
-    },
-    include: [
+//TODO: remove comment
+router.get(
+  '/',
+  /*withAuth,*/ (req, res) => {
+    /*Lesson.findAll({
+      where: {
+        student_id: req.session.student_id,
+      },
+      include: [
+        {
+          model: Weekly_Timeslot,
+          attributes: ['teacher_id', 'day'],
+          include: {
+            model: Teacher,
+            attributes: ['first_name', 'last_name'],
+          },
+        },
+      ],
+    }
+  )
+  .then((dbLessonData) => {
+    //TODO: remove comment
+    //const lessons = dbLessonData.map((lesson) => lesson.get({ plain: true }));
+
+    //TODO: remove dummy data
+    */
+    const lessons = [
       {
-        model: Weekly_Timeslot,
-        attributes: ['teacher_id', 'day'],
-        include: {
-          model: Teacher,
-          attributes: ['first_name', 'last_name'],
+        weekly_timeslot: {
+          start_time: '13:00:00',
+          end_time: '13:59:00',
+          teacher_id: 1,
+          day: 1,
         },
       },
-    ],
-  })
-    .then((dbLessonData) => {
-      const lessons = dbLessonData.map((lesson) => lesson.get({ plain: true }));
+      {
+        weekly_timeslot: {
+          start_time: '14:00:00',
+          end_time: '14:59:00',
+          teacher_id: 1,
+          day: 2,
+        },
+      },
+      {
+        weekly_timeslot: {
+          start_time: '15:00:00',
+          end_time: '15:59:00',
+          teacher_id: 1,
+          day: 3,
+        },
+      },
+    ];
 
-      res.render('dashboard', {
-        lessons,
-        loggedIn: req.session.loggedIn,
-        currentUser: req.session.first_name,
-      });
-    })
-    .catch((err) => {
-      console.log(err);
-      res.sendStatus(500).json(err);
+    // format times to 12 hour
+    lessons.forEach((lesson) => {
+      const time = DateTime.fromSQL(
+        lesson.weekly_timeslot.start_time
+      ).toLocaleString(DateTime.TIME_SIMPLE);
+
+      lesson.weekly_timeslot.start_time = time;
     });
-});
+
+    res.render('dashboard', {
+      lessons,
+      loggedIn: req.session.loggedIn,
+      currentUser: req.session.first_name,
+    });
+  }
+);
+// .catch((err) => {
+//   console.log(err);
+//   res.sendStatus(500).json(err);
+// });
+// }
+//);
 
 module.exports = router;
--- utils/helpers.js
+++ utils/helpers.js
@@ -1,3 +1,5 @@
 function checkDay(dayOne, dayTwo) {
   return dayOne === dayTwo ? true : false;
 }
+
+module.exports = { checkDay };
