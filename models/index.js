e1f4a812cfc195b745b204775278fd3092ae08bf
kcbryan <kcbryan10@gmail.com>
2022-01-04 17:26:28 -0600

    added belongto for each has statement

--- models/index.js
+++ models/index.js
@@ -3,8 +3,11 @@ const Lesson = require('./Lesson');
 const Teacher = require('./teacher');
 const Student = require('./student');
 const Weekly_Timeslot = require('./Weekly_Timeslot');
-const Specialties = require('./Specialties')
+const Specialties = require('./Specialties');
+const { template } = require('handlebars');
 
+
+// User
 User.hasOne(Student, {
     foreignKey: 'user_id'
 });
@@ -13,20 +16,58 @@ User.hasOne(Teacher, {
     foreignKey: 'user_id',
 });
 
+// Student
+
+Student.belongsTo(User, {
+    foreignKey:'user_id'
+});
+
 Student.hasMany(Lesson, {
     foreignKey: 'student_id'
+});
+
+// Teacher
+
+Teacher.belongsTo(User, {
+    foreignKey:'user_id'
 });
 
 Teacher.hasMany(Weekly_Timeslot, {
     foreignKey:'teacher_id'
 });
 
+Teacher.hasMany(Specialties, {
+    foreignKey: 'specialty_name'
+})
+
+
+
+// Weekly Timeslot 
+
+Weekly_Timeslot.belongsTo(Teacher, {
+    foreignKey: 'teacher_id'
+})
+
 Weekly_Timeslot.hasMany(Lesson, {
     foreignKey: 'timeslot_id'
 });
 
-Teacher.hasMany(Specialties, {
-    foreignKey: 'specialty_name'
+
+// Lesson 
+
+Lesson.belongsTo(Weekly_Timeslot, {
+    foreignKey:'timeslot_id'
+});
+
+Lesson.belongsTo(Student, {
+    foreignKey: 'student_id'
+});
+
+
+// Specialties 
+
+Specialties.belongsTo(Teacher, {
+    foreignKey: 'teacher_id'
 })
 
 
