1100d247f3f109427e9a475857137c7eacd302dd
Naomi-Eckhoff <nsuibhne@gmail.com>
2022-01-05 00:04:40 -0600

    connected existing apis

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,11 @@
 const router = require('express').Router();
 
 const lessonRoutes = require('./lesson-routes');
+const userRoutes = require('./users-routes');
+const weeklyTimeslotRoutes = require('./weekly_timeslot-routes');
 
 router.use('/lessons', lessonRoutes);
+router.use('/users', userRoutes);
+router.use('/weeklytimeslots', weeklyTimeslotRoutes);
 
 module.exports = router;
